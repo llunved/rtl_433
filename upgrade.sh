1018b5c65f8def9fa1ab5233882899ee4b534c26
Daniel Riek <riek@llnvd.io>
2020-05-01 21:02:55 -0400

    Making upgrade work

--- upgrade.sh
+++ upgrade.sh
@@ -14,7 +14,8 @@ if [ ! -d /host/etc -o ! -d /host/proc -o ! -d /host/var/run ]; then
 fi
 
 
-chroot /host sh -c "/usr/bin/systemctl stop $NAME} && sleep 30 && /usr/bin/podman rm ${NAME} && sleep 15"
+chroot /host sh -c "/usr/bin/systemctl stop ${NAME} && sleep 30 && /usr/bin/podman rm ${NAME} && sleep 15"
 chroot /host /usr/bin/podman create --name ${NAME} --privileged --net=host --device /dev/dvb:rw --entrypoint /sbin/entrypoint.sh -v ${DATADIR}/${NAME}:/var/lib/rtl_433:rw -v ${CONFDIR}/${NAME}:/etc/rtl_433:rw -v ${LOGDIR}/${NAME}:/var/log/rtl_433:rw ${IMAGE} /bin/start.sh
 chroot /host sh -c "/usr/bin/podman generate systemd --restart-policy=always -t 1 ${NAME} > /etc/systemd/system/${NAME}.service && systemctl daemon-reload"
+chroot /host sh -c "/usr/bin/systemctl start ${NAME} "
 
